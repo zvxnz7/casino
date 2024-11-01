251e7c310156860198b0e308dc8c65c7786f89f4
zvxnz7 <zvxnz7@gmail.com>
2024-11-01 16:28:35 +0100

    Update games.js

--- games.js
+++ games.js
@@ -1,1 +1,12 @@
+const gameCards = document.querySelectorAll('.game-card');
+const money = localStorage.getItem('userMoney');
 
+
+gameCards.forEach(card => {
+    const gameName = card.getAttribute('data-game');
+
+    card.addEventListener('click', () => {
+        window.location.href = 'mines.html';
+
+    });
+});
